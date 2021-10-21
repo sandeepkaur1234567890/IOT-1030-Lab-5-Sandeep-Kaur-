8ee695f0cd184f733b2689f219e096c84026f645
sandeepkaur1234567890 <78116974+sandeepkaur1234567890@users.noreply.github.com>
2021-10-21 10:49:58 -0400

    commit

---
+++ IOT 1030 Lab 5 Sandeep Kaur/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+using IOT_1030_Lab5_Sandeep_Kaur.Particles;
+using IOT_1030_Lab5_Sandeep_Kaur.ModelComponents;
+using IOT_1030_Lab5_Sandeep_Kaur.Materials;
+
+namespace IOT_1030_Lab5_Sandeep_Kaur
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DispersionData dData;
+			dData.LaData = new double[] { -2.22e-7, 9260.0, 0.0 };
+			dData.TaData = new double[] { -2.28e-7, 5240.0, 0.0 };
+			dData.WMaxLa = 7.63916048e13;
+			dData.WMaxTa = 3.0100793072e13;
+
+			RelaxationData rData;
+			rData.Bl = 1.3e-24;
+			rData.Btn = 9e-13;
+			rData.Btu = 1.9e-18;
+			rData.BI = 1.2e-45;
+			rData.W = 2.42e13;
+
+			Material silicon = new Material(in dData, in rData);
+
+			Sensor s1 = new Sensor(1, silicon, 300);
+			Cell c1 = new Cell(10, 10, s1);
+			Sensor s2 = new Sensor(2, silicon, 300);
+			Cell c2 = new Cell(1, 1, s2);
+
+
+			c1.SetEmitSurface(SurfaceLocation.left, c1, 300);
+			c1.SetEmitSurface(SurfaceLocation.right, c1, 300);
+			c1.SetEmitSurface(SurfaceLocation.bot, c1, 300);
+			c1.SetEmitSurface(SurfaceLocation.top, c1, 300);
+			c1.EmitEnergy(300, 1);
+
+			// Test transition surface (HandlePhonon)
+			Console.WriteLine("\t\t testing transition surface");
+			Phonon p1 = new Phonon(1);
+			double px = 10;
+			p1.SetCoords(px, 1);
+			p1.SetDirection(0.5, 0.5);
+			Console.WriteLine($"Position before {px}");
+			TransitionSurface ts = new TransitionSurface(SurfaceLocation.right, c2);
+			Cell c3 = ts.HandlePhonon(p1);
+			p1.GetCoords(out px, out double py);
+			Console.WriteLine($"Position after {px}");
+			Console.WriteLine($"New Cell: {c3}");
+
+
+			// Test emit surface (HandlePhonon)
+			Console.WriteLine("\t\t testing transition surface");
+			Phonon p2 = new Phonon(1);
+			p2.DriftTime = 10;
+
+			Console.WriteLine($"Phonon properties prior to emit surface collision");
+
+			Console.WriteLine($"Active: {p2.Active}");
+			Console.WriteLine($"Drift Time: {p2.DriftTime}");
+
+			EmitSurface es = new EmitSurface(SurfaceLocation.left, c1, 300);
+			es.HandlePhonon(p2);
+
+			Console.WriteLine($"Phonon properties after to emit surface collision");
+			Console.WriteLine(p2.Active);
+			Console.WriteLine(p2.DriftTime);
+		}
+	}
+}
---
+++ IOT 1030 Lab 5 Sandeep Kaur/Surface.cs
@@ -0,0 +1,107 @@
+﻿using IOT_1030_Lab5_Sandeep_Kaur.Particles;
+using IOT_1030_Lab5_Sandeep_Kaur.Geometry2D;
+using System;
+
+namespace IOT_1030_Lab5_Sandeep_Kaur.ModelComponents
+{
+	public interface ISurface
+	{
+		Cell HandlePhonon(Phonon p);
+	}
+
+	/// <summary>
+	/// When a phonon collides with a boundary surface, it undergoes specular reflection
+	/// and is redirected back into its current cell
+	/// </summary>
+	public class BoundarySurface : ISurface
+	{
+		public SurfaceLocation Location { get; }
+		protected Cell cell;
+
+		public BoundarySurface(SurfaceLocation location, Cell cell)
+		{
+			Location = location;
+			this.cell = cell;
+		}
+		public virtual Cell HandlePhonon(Phonon p)
+		{
+			Vector direction = p.Direction;
+			if (Location == SurfaceLocation.left || Location == SurfaceLocation.right)
+			{
+				p.SetDirection(-direction.DX, direction.DY);
+			}
+			else
+			{
+				p.SetDirection(direction.DX, -direction.DY);
+			}
+			return cell;
+		}
+	}
+
+	public class TransitionSurface : BoundarySurface
+	{
+		//TODO: Implement constructor
+
+		public TransitionSurface(SurfaceLocation location, Cell cell) : base(location, cell)
+		{
+		}
+
+		/// <summary>
+		/// When a phonon collides with a transition surface, it passes from
+		/// one cell to the adjacent cell. This method updates the phonon's coordinates
+		/// to be on the border of the new cell. The new cell is returned.
+		/// </summary>
+		/// <param name="p">The phonon that will be moved from one cell to another</param>
+		/// <returns>The new cell in which the phonon will reside</returns>
+		public override Cell HandlePhonon(Phonon p)
+		{
+			//TODO: Implement
+			throw new NotImplementedException();
+		}
+	}
+
+	public class EmitSurface : BoundarySurface
+	{
+		private readonly double emitEnergy;
+		public Tuple<double, double>[] EmitTable { get; }
+		public double Temp { get; }
+		public int EmitPhonons { get; private set; }
+		public double EmitPhononsFrac { get; private set; }
+		public EmitSurface(SurfaceLocation location, Cell cell, double temp) : base(location, cell)
+		{
+			Temp = temp;
+			EmitTable = cell.EmitData(temp, out emitEnergy);
+		}
+
+		/// <summary>
+		/// If a phonon collides with an emitting surface it leaves the system and
+		/// is no longer part of the simulation
+		/// </summary>
+		/// <param name="p">The phonon that will be removed from the simulation</param>
+		/// <returns>The cell the phonon resided in prior to being removed from the simulation</returns>
+		public override Cell HandlePhonon(Phonon p)
+		{
+			// TODO: implement
+			throw new NotImplementedException();
+		}
+		public double GetEmitEnergy(double tEq, double simTime, double length)
+		{
+			return emitEnergy * length * simTime / 4 * Math.Abs(Temp - tEq);
+		}
+
+		/// <summary>
+		/// Sets the number of phonons this surface will emit during each simulation time step
+		/// </summary>
+		/// <param name="tEq">The equilibrium temperature of the system</param>
+		/// <param name="effEnergy">The effective energy of each phonon packet</param>
+		/// <param name="timeStep">The simulation time step</param>
+		/// <param name="length">The dimension of the surface (1D here)</param>
+		public void SetEmitPhonons(double tEq, double effEnergy, double timeStep, double length)
+		{
+			double emitPhonons = GetEmitEnergy(tEq, timeStep, length) / effEnergy;
+			//TODO: Implement -> need to split the double emitPhonons into it's
+			// integer component (EmitPhonons) and its
+			// fractional component (EmitPhononsFrac)
+		}
+	}
+}
